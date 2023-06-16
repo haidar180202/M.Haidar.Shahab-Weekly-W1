53f2ffc5fd96a066eae3bb93af65eb011472ec4b
unknown <gshaidar6@gmail.com>
2023-06-16 23:40:30 +0700

    replace class to className

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <div className='text-blue-300 lg:ml-44 mt-10 md:m-0 mr-1'>
           <p className='mt-8 inline-grid'>Hello! My Name Is<span className='font-bold text-blue-700'>M.Haidar Shahab</span></p>
           <p>Front End Developer</p>
-          <button onClick={()=>(alert("Terima kasih Banyak"))} class="rounded-md my-2 p-3 bg-blue-300 text-white text-sm font-semibold">Support Me</button>
+          <button onClick={()=>(alert("Terima kasih Banyak"))} className="rounded-md my-2 p-3 bg-blue-300 text-white text-sm font-semibold">Support Me</button>
           <div className='flex flex-row mt-2 mx-auto'>
             <a href='https://github.com/haidar180202'><img src={githublogo} className="w-15 h-12"/></a>
             <a href='https://www.linkedin.com/in/muhammad-haidar-shahab-bb9338212/'><img src={linkedinlogo} className="w-12 mr-2"/></a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Navbar() {
   return (
     <div className="w-full flex flex-row bg-blue-950">
-      <div class="basic-1/6 ml-10 mr-32 py-6 font-bold text-blue-300">HAIDAR SHAHAB</div>
-      <a href="" class="basis-1/6  py-6 font-bold text-blue-300">Home</a>
-      <a href ="" class="basis-1/6 py-6 font-bold text-blue-300">About</a>
+      <div className="basic-1/6 ml-10 mr-32 py-6 font-bold text-blue-300">HAIDAR SHAHAB</div>
+      <a href="" className="basis-1/6  py-6 font-bold text-blue-300">Home</a>
+      <a href ="" className="basis-1/6 py-6 font-bold text-blue-300">About</a>
     </div>
   );
 }
