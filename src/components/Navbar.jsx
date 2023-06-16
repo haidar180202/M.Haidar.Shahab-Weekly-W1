dcda4a714170226fba2eb21df4a63572e6e1f577
unknown <gshaidar6@gmail.com>
2023-06-16 23:01:18 +0700

    add href in about

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div className="w-full flex flex-row bg-blue-950">
       <div class="basic-1/6 ml-10 mr-32 py-6 font-bold text-blue-300">HAIDAR SHAHAB</div>
       <a href="" class="basis-1/6  py-6 font-bold text-blue-300">Home</a>
-      <a class="basis-1/6 py-6 font-bold text-blue-300">About</a>
+      <a href ="" class="basis-1/6 py-6 font-bold text-blue-300">About</a>
     </div>
   );
 }
